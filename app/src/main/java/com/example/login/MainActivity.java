2108804643b81b10797ee704b47446498840aeb1
Rustam <sin15a@mail.ru>
2019-03-07 17:47:14 +0300

    Create the new animation on the main button

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.login;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +19,18 @@ public class MainActivity extends AppCompatActivity {
    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Animation animAlpha = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                view.startAnimation(animAlpha);
+            }
+        });
+
        // int i=0;
-       Button create_accaunt=(Button)findViewById(R.id.button1);
+       Button create_accaunt = (Button)findViewById(R.id.button1);
       //  int i = 1;
       // while ( i == 1 ){
          create_accaunt.setOnClickListener(new View.OnClickListener()
@@ -40,3 +53,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+    /*final Animation Animalpha = AnimationUtils.loadAnimation(this, R.anim.alpha);
+    Button button = (Button)findViewById(R.id.alpha);
+        button.setOnClickListener(new Button.OnClickListener(){
+@Override
+public void onClick(View view){
+        view.startAnimation(animAlpha);
+        }
+        }*/
