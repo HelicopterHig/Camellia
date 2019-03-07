b0ac70c782222f8eb1fa8b933ea39923333148e1
TBSjiga <Dima9905@yandex.ru>
2019-03-07 12:08:57 +0300

    This is new Style of button "Создать аккаунт"

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -4,18 +4,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
 
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+   protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView create_accaunt=(TextView)findViewById(R.id.textView);
-        create_accaunt.setOnClickListener(new View.OnClickListener() {
+       // int i=0;
+       Button create_accaunt=(Button)findViewById(R.id.button1);
+      //  int i = 1;
+      // while ( i == 1 ){
+         create_accaunt.setOnClickListener(new View.OnClickListener()
+        {
 
             public void onClick(View v) {
                 setContentView(R.layout.activity_main1);
@@ -30,5 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+       //}
     }
+
 }
