e6d0e40df330091816d946d1cc386e3e3f21322e
Rustam <sin15a@mail.ru>
2019-03-09 11:24:43 +0300

    New! Экран не переходит в Lanscape ориентацию!

--- app/src/main/java/com/example/login/Activity2.java
+++ app/src/main/java/com/example/login/Activity2.java
@@ -1,6 +1,7 @@
 package com.example.login;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +16,7 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
 
         final Animation animAlpha = AnimationUtils.loadAnimation(this, R.anim.alpha);
--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,4 +1,6 @@
 package com.example.login;
+
+import android.content.pm.ActivityInfo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
 
 
 
