1134c8819f846e53b236131bcb3c48312d8efade
TBSjiga <Dima9905@yandex.ru>
2019-04-03 14:13:36 +0300

    New! Версия 0.3.2 Добавлена проверка на валидность email.

---
+++ app/src/main/java/com/example/login/EmailValidator.java
@@ -0,0 +1,25 @@
+package com.example.login;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+// Проверка валидности email
+
+public class EmailValidator {
+    private Pattern pattern;
+    private Matcher matcher;
+
+    private static final String EMAIL_PATTERN =
+            "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +
+                    "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+    public EmailValidator() {
+        pattern = Pattern.compile(EMAIL_PATTERN);
+    }
+
+    public boolean validate(final String hex) {
+        matcher = pattern.matcher(hex);
+
+        return matcher.matches();
+    }
+}
