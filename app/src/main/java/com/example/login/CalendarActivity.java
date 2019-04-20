349c932fbe7363f26249f3b6e786baf44f2f0f0a
Nikita <nickita13.01.2000@yandex.ru>
2019-04-20 20:07:15 +0300

    New! 0.4.16 Добавлен личный календарь с заметками

--- app/src/main/java/com/example/login/CalendarActivity.java
+++ app/src/main/java/com/example/login/CalendarActivity.java
@@ -1,22 +1,188 @@
 package com.example.login;
 
-
-import android.content.pm.ActivityInfo;
-import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
-import com.r0adkll.slidr.Slidr;
-import com.r0adkll.slidr.model.SlidrInterface;
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
 
-public class  CalendarActivity extends AppCompatActivity {
-    private SlidrInterface slidr;
+
+public class CalendarActivity extends AppCompatActivity {
+    public GregorianCalendar cal_month, cal_month_copy;
+    private HwAdapter hwAdapter;
+    protected String  desctiprion, type, n_class, data;
+    private TextView tv_month;
+    private Button button;
+    protected String name;
+    EditText edit1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        slidr = Slidr.attach(this);
+/*
+        String languageToLoad  = "RUSSIAN"; // your language
+        Locale locale = new Locale(languageToLoad);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale.GERMAN;
+        getBaseContext().getResources().updateConfiguration(config,
+                getBaseContext().getResources().getDisplayMetrics());
+        setContentView(R.layout.activity_calendar);
+*/
+
+
+        HomeCollection.date_collection_arr=new ArrayList<HomeCollection>();
+        HomeCollection.date_collection_arr.add( new HomeCollection("2019-07-08" ,"Diwali","Holiday","this is holiday"));
+        HomeCollection.date_collection_arr.add( new HomeCollection("2019-07-08" ,"Holi","Holiday","this is holiday"));
+        HomeCollection.date_collection_arr.add( new HomeCollection("2019-07-08" ,"Statehood Day","Holiday","this is holiday"));
+        HomeCollection.date_collection_arr.add( new HomeCollection("2019-08-08" ,"Republic Unian","Holiday","this is holiday"));
+        HomeCollection.date_collection_arr.add( new HomeCollection("2019-07-09" ,"ABC","Holiday","this is holiday"));
+        HomeCollection.date_collection_arr.add( new HomeCollection("2019-06-15" ,"demo","Holiday","this is holiday"));
+        HomeCollection.date_collection_arr.add( new HomeCollection("2019-09-26" ,"weekly off","Holiday","this is holiday"));
+        HomeCollection.date_collection_arr.add( new HomeCollection("2019-01-08" ,"Events","Holiday","this is holiday"));
+        HomeCollection.date_collection_arr.add( new HomeCollection("2019-01-16" ,"Dasahara","Holiday","this is holiday"));
+        HomeCollection.date_collection_arr.add( new HomeCollection("2019-02-09" ,"Christmas","Holiday","this is holiday"));
+
+
+
+        cal_month = (GregorianCalendar) GregorianCalendar.getInstance();
+        cal_month_copy = (GregorianCalendar) cal_month.clone();
+        hwAdapter = new HwAdapter(this, cal_month,HomeCollection.date_collection_arr);
+
+        tv_month = (TextView) findViewById(R.id.tv_month);
+        tv_month.setText(android.text.format.DateFormat.format("MMMM yyyy", cal_month));
+
+
+        ImageButton previous = (ImageButton) findViewById(R.id.ib_prev);
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cal_month.get(GregorianCalendar.MONTH) == 4&&cal_month.get(GregorianCalendar.YEAR)==2000) {
+                    //cal_month.set((cal_month.get(GregorianCalendar.YEAR) - 1), cal_month.getActualMaximum(GregorianCalendar.MONTH), 1);
+                    Toast.makeText(CalendarActivity.this, "Event Detail is available for current session only.", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    setPreviousMonth();
+                    refreshCalendar();
+                }
+
+
+            }
+        });
+        ImageButton next = (ImageButton) findViewById(R.id.Ib_next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cal_month.get(GregorianCalendar.MONTH) == 5&&cal_month.get(GregorianCalendar.YEAR)==3000) {
+                    //cal_month.set((cal_month.get(GregorianCalendar.YEAR) + 1), cal_month.getActualMinimum(GregorianCalendar.MONTH), 1);
+                    Toast.makeText(CalendarActivity.this, "Event Detail is available for current session only.", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    setNextMonth();
+                    refreshCalendar();
+                }
+            }
+        });
+        GridView gridview = (GridView) findViewById(R.id.gv_calendar);
+        gridview.setAdapter(hwAdapter);
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                String selectedGridDate = HwAdapter.day_string.get(position);
+                ((HwAdapter) parent.getAdapter()).getPositionList(selectedGridDate, CalendarActivity.this);
+            }
+
+        });
+
+
+        button = (Button) findViewById(R.id.button_node_c);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder mBuilder = new AlertDialog.Builder(CalendarActivity.this);
+                final View mView = getLayoutInflater().inflate(R.layout.row_adapt_create, null);
+
+                Button mLogin = (Button) mView.findViewById(R.id.button_node_c_1);
+                Button mLogin22 = (Button) mView.findViewById(R.id.button_exit_c);
+
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+
+                mLogin.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        EditText editText_0 = (EditText) mView.findViewById(R.id.tv_data_c);
+                        data = String.valueOf(editText_0.getText().toString());
+
+                        EditText editText = (EditText) mView.findViewById(R.id.tv_name_c);
+                        name = String.valueOf(editText.getText().toString());
+
+                        EditText editText_1 = (EditText) mView.findViewById(R.id.tv_type_c);
+                        type = String.valueOf(editText_1.getText().toString());
+
+                        EditText editText_2 = (EditText) mView.findViewById(R.id.tv_class_c);
+                        n_class = String.valueOf(editText_2.getText().toString());
+
+
+                        HomeCollection.date_collection_arr.add( new HomeCollection(data ,name, type,n_class));
+
+                        //HomeCollection.date_collection_arr.add( new HomeCollection("2019-07-08" ,"Holi","Holiday","this is holiday"));
+
+                        dialog.dismiss();
+                    }
+                });
+
+                mLogin22.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+
+
+            }
+        });
+
+    }
+    protected void setNextMonth() {
+        if (cal_month.get(GregorianCalendar.MONTH) == cal_month.getActualMaximum(GregorianCalendar.MONTH)) {
+            cal_month.set((cal_month.get(GregorianCalendar.YEAR) + 1), cal_month.getActualMinimum(GregorianCalendar.MONTH), 1);
+        } else {
+            cal_month.set(GregorianCalendar.MONTH,
+                    cal_month.get(GregorianCalendar.MONTH) + 1);
+        }
+    }
+
+    protected void setPreviousMonth() {
+        if (cal_month.get(GregorianCalendar.MONTH) == cal_month.getActualMinimum(GregorianCalendar.MONTH)) {
+            cal_month.set((cal_month.get(GregorianCalendar.YEAR) - 1), cal_month.getActualMaximum(GregorianCalendar.MONTH), 1);
+        } else {
+            cal_month.set(GregorianCalendar.MONTH, cal_month.get(GregorianCalendar.MONTH) - 1);
+        }
+    }
+
+    public void refreshCalendar() {
+        hwAdapter.refreshDays();
+        hwAdapter.notifyDataSetChanged();
+        tv_month.setText(android.text.format.DateFormat.format("MMMM yyyy", cal_month));
     }
 }
